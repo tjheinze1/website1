d00853465f91c3c5aec781ed29ec32fb2d9e9717
tjheinze1 <142804675+tjheinze1@users.noreply.github.com>
2024-03-15 21:51:29 +0000

    Fri, Mar 15, 2024, 2:51 PM -07:00

--- loading.js
+++ loading.js
@@ -14,8 +14,8 @@ function blurring() {
     }
     loadText.innerText = '${load}%
 
-    
-    loadText.style.opacity = 1
-bg.style.filter = "blur(10px)"
+
+    loadText.style.opacity = 1 - load/100
+    bg.style.filter = "blur(10px)"
 }
 
