211c7395a96a12bfec64b71692bd96ecb4160d97
tjheinze1 <142804675+tjheinze1@users.noreply.github.com>
2024-03-15 21:50:09 +0000

    Fri, Mar 15, 2024, 2:50 PM -07:00

--- loading.js
+++ loading.js
@@ -1,1 +1,21 @@
-e]
+loadText = document.querySelector('.loading-text')
+bg = document.querySelector('.bg )
+
+load = 0
+
+int = setInterval(blurring, 30)
+
+
+function blurring() {
+    load++
+
+    if (load > 99){
+        clearInterval(int)
+    }
+    loadText.innerText = '${load}%
+
+    
+    loadText.style.opacity = 1
+bg.style.filter = "blur(10px)"
+}
+
